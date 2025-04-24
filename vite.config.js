c59e831548829f869b0ca8258efe8cdf29569c86
AdarshJaso <adarsh.jasoliya@gmail.com>
2025-04-24 12:23:34 +1000

    added tailwindcss for vite, fixed tailwindcss imports

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), tailwindcss()],
   server: {
     port: 3000,
     open: true, // Automatically open the browser
